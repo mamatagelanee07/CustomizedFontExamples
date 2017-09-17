23d3732633ac8f6359f218e9e36d5e5ae1eae166
mamatagelanee07 <gelaneem@gmail.com>
2017-09-17 19:23:40 +0530

    applied typeface to textviews

--- app/src/main/java/com/andigeeky/customizedfontexamples/fragments/LegacyFragment.java
+++ app/src/main/java/com/andigeeky/customizedfontexamples/fragments/LegacyFragment.java
@@ -1,11 +1,13 @@
 package com.andigeeky.customizedfontexamples.fragments;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.andigeeky.customizedfontexamples.R;
 
@@ -19,6 +21,28 @@ public class LegacyFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_legacy, container, false);
+        View view = inflater.inflate(R.layout.fragment_legacy, container, false);
+        applyCustomFonts(view);
+        return view;
+    }
+
+    private void applyCustomFonts(View view) {
+        TextView textFont1 = view.findViewById(R.id.textFont1);
+        TextView textFont2 = view.findViewById(R.id.textFont2);
+        TextView textFont3 = view.findViewById(R.id.textFont3);
+        TextView textFont4 = view.findViewById(R.id.textFont4);
+        TextView textFont5 = view.findViewById(R.id.textFont5);
+
+        Typeface typeface1 = Typeface.createFromAsset(getActivity().getAssets(), "EncodeSansExpanded-Light.ttf");
+        Typeface typeface2 = Typeface.createFromAsset(getActivity().getAssets(), "EncodeSansExpanded-Regular.ttf");
+        Typeface typeface3 = Typeface.createFromAsset(getActivity().getAssets(), "EncodeSansExpanded-Medium.ttf");
+        Typeface typeface4 = Typeface.createFromAsset(getActivity().getAssets(), "EncodeSansExpanded-SemiBold.ttf");
+        Typeface typeface5 = Typeface.createFromAsset(getActivity().getAssets(), "EncodeSansExpanded-Bold.ttf");
+
+        textFont1.setTypeface(typeface1);
+        textFont2.setTypeface(typeface2);
+        textFont3.setTypeface(typeface3);
+        textFont4.setTypeface(typeface4);
+        textFont5.setTypeface(typeface5);
     }
 }
