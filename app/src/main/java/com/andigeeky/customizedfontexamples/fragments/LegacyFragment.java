1707ba6af0364ac644f64b49ca8f1b5c39cd1f45
mamatagelanee07 <gelaneem@gmail.com>
2017-09-17 18:22:46 +0530

    Added legacy code fragment and layout file

---
+++ app/src/main/java/com/andigeeky/customizedfontexamples/fragments/LegacyFragment.java
@@ -0,0 +1,24 @@
+package com.andigeeky.customizedfontexamples.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.andigeeky.customizedfontexamples.R;
+
+/**
+ * Created by Mamata on 9/17/2017.
+ */
+
+public class LegacyFragment extends Fragment {
+    public static final String TAG = LegacyFragment.class.getSimpleName();
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_legacy, container, false);
+    }
+}
